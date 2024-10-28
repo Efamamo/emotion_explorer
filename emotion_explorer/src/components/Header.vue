543ab1ff5b13ea84df04d132d48d1b9629ff2d72
ephrem <ephremmamo555@gmail.com>
2024-10-28 10:24:03 +0300

    add mobile nav

---
+++ emotion_explorer/src/components/Header.vue
@@ -0,0 +1,105 @@
+<script setup>
+import { RouterLink, RouterView, useRoute } from 'vue-router';
+const route = useRoute();
+
+function isActive(path) {
+  if (path === '/') {
+    return path === route.path;
+  }
+  return route.path.includes(path);
+}
+import { ref } from 'vue';
+import Sheet from './Sheet.vue';
+
+const isSheetOpen = ref(false);
+
+function toggleSheet() {
+  isSheetOpen.value = !isSheetOpen.value;
+}
+</script>
+
+<template>
+  <header class="hidden md:block">
+    <div>
+      <nav class="flex gap-3 justify-between px-16 items-center mt-8 mb-5">
+        <RouterLink to="/" class="flex-1">
+          <img src="/studying.png" alt="" class="w-10" />
+        </RouterLink>
+        <div class="flex gap-5 justify-center flex-1">
+          <RouterLink
+            to="/"
+            class="font-semibold text-lg"
+            :class="isActive('/') ? 'border-b text-blue-600' : ''"
+            >Home</RouterLink
+          >
+          <RouterLink
+            to="/about"
+            class="font-semibold text-lg"
+            :class="isActive('/about') ? 'border-b text-blue-600' : ''"
+            >About</RouterLink
+          >
+
+          <RouterLink
+            to="/features"
+            class="font-semibold text-lg"
+            :class="isActive('/features') ? 'border-b text-blue-600' : ''"
+            >Features</RouterLink
+          >
+        </div>
+        <div class="flex gap-4 items-center flex-1 justify-end">
+          <RouterLink to="/login" class="font-semibold text-lg">
+            Login
+          </RouterLink>
+          <RouterLink
+            to="/signup"
+            class="font-semibold text-lg bg-black text-white px-6 py-2 rounded-full"
+          >
+            Signup
+          </RouterLink>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <div class="relative md:hidden flex items-center justify-end p-4">
+    <RouterLink to="/" class="block flex-1 md:hidden">
+      <img src="/studying.png" alt="" class="w-10" />
+    </RouterLink>
+    <img src="/burger.svg" @click="toggleSheet" class="w-10 block md:hidden" />
+
+    <Sheet :isOpen="isSheetOpen" :onClose="toggleSheet">
+      <h2 class="text-2xl font-bold text-center mb-6">Smart Study</h2>
+      <div class="flex flex-col gap-10">
+        <div class="flex flex-col gap-5 justify-center flex-1">
+          <RouterLink
+            to="/"
+            class="font-semibold text-lg"
+            :class="isActive('/') ? 'border-b text-blue-600' : ''"
+            >Home</RouterLink
+          >
+          <RouterLink
+            to="/about"
+            class="font-semibold text-lg"
+            :class="isActive('/about') ? 'border-b text-blue-600' : ''"
+            >About</RouterLink
+          >
+
+          <RouterLink
+            to="/features"
+            class="font-semibold text-lg"
+            :class="isActive('/features') ? 'border-b text-blue-600' : ''"
+            >Features</RouterLink
+          >
+        </div>
+        <div class="flex gap-4 items-start flex-1 flex-col">
+          <RouterLink to="/login" class="font-semibold text-lg">
+            Login
+          </RouterLink>
+          <RouterLink to="/signup" class="font-semibold text-lg">
+            Signup
+          </RouterLink>
+        </div>
+      </div>
+    </Sheet>
+  </div>
+</template>
---
+++ emotion_explorer/src/components/Sheet.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    v-if="isOpen"
+    class="md:hidden fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+  >
+    <div
+      @click="close"
+      class="sheet fixed top-0 right-0 bottom-0 max-w-sm bg-white p-4 w-full shadow-lg transform transition-transform duration-1000 ease-in-out"
+      :class="{ 'translate-x-0': isOpen, 'translate-x-full': !isOpen }"
+    >
+      <!-- Close Button -->
+      <button class="absolute top-4 right-4 text-gray-500 hover:text-gray-800">
+        ✕
+      </button>
+
+      <!-- Slot for Sheet Content -->
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// Props
+const props = defineProps({
+  isOpen: Boolean,
+  onClose: Function,
+});
+
+// Methods
+function close() {
+  props.onClose();
+}
+</script>
+
+<style scoped>
+.sheet {
+  will-change: transform;
+  transition: transform 0.5s ease-in-out;
+}
+</style>
